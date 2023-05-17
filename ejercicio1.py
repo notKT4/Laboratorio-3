b1104481665d7e41c706a5deba203f19e407e234
dolchKT4 <120228067+notKT4@users.noreply.github.com>
2023-05-17 15:27:18 +0100

    control

---
+++ ejercicio1.py
@@ -0,0 +1,33 @@
+
+superficie1=23890
+superficie2=48583
+superficie=superficie1,superficie2
+
+region="Biobio"
+region1="Los_Lagos"
+
+habitantes1=1556805 
+habitantes2=828708
+habitantes=habitantes1,habitantes2
+
+dict={
+    8:[region,superficie,habitantes],
+    10:[region1,superficie1,habitantes1]
+}
+print(dict)
+
+#densidad= round(habitantes/superficie)
+
+capitalbio=["concepcion"]
+capitallag=["puerto montt"]
+capital=capitalbio,capitallag
+
+comunasbio=["lota","lebu","los angeles"]
+comunaslag=["castro","puerto varas","purranque"]
+comunas=comunasbio,comunaslag
+
+provinciasbio=("bio-bio","arauco","concepcion")
+provinciaslag=("osorno","llanquihue","chiloé","palena")
+provincias=provinciasbio,provinciaslag
+
+#print(f"Nuevo diccionario: {dict}")
